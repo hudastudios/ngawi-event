711c76b782ace7d03446b57d8555dddf3a2252ea
hudastudios <studios.huda@gmail.com>
2026-02-05 09:56:38 +0700

    fix(web): restore messed up vite config

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-plugins: [react()],
+export default defineConfig({
+  plugins: [react()],
   build: {
-  outDir: 'build'
-}
+    outDir: 'build'
+  }
 })
